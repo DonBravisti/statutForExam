f1b7aa9a231c21c1f8167636b57c4b7e605f2228
DonBravisti <butov.s-butov@yandex.ru>
2023-01-23 17:48:27 +0300

    Добавил оставщиеся слайдеры

--- script.js
+++ script.js
@@ -4,4 +4,29 @@ $(document).ready(function () {
         autoplay: true,
         arrows: true
     });
+
+    $('.new-arrivals-cards').slick({
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        prevArrow: $('.prev-arrivals'),
+        nextArrow: $('.next-arrivals')
+    });
+
+    $('.publishing-cards').slick({
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        prevArrow: $('.prev-publishing'),
+        nextArrow: $('.next-publishing')
+    });
+
+    $('.news-items').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: $('.prev-news'),
+        nextArrow: $('.next-news')
+    });
 });
+
